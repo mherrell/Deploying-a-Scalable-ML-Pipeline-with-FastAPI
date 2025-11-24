cd5d1b48360afaf06badbce11f03c797c043f3e6
mherrell <mherr@LAPTOP-FSB07L4G.localdomain>
2025-11-24 12:52:47 -0500

    debug

--- ml/data.py
+++ ml/data.py
@@ -57,11 +57,11 @@ def process_data(
         encoder = OneHotEncoder(sparse_output=False, handle_unknown="ignore")
         lb = LabelBinarizer()
         X_categorical = encoder.fit_transform(X_categorical)
-        y = lb.fit_transform(y).ravel()
+        y = lb.fit_transform(y.values).ravel()
     else:
         X_categorical = encoder.transform(X_categorical)
         try:
-            y = lb.transform(y).ravel()
+            y = lb.transform(y.values).ravel()
         # Catch the case where y is None because we're doing inference.
         except AttributeError:
             pass
